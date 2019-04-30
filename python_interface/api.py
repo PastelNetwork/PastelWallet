8f2b6edc14a8a18f8042b3f83a07c896b9cca3e3
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-04-30 11:48:53 +0300

    Multiple key generation support

--- python_interface/api.py
+++ python_interface/api.py
@@ -1,6 +1,18 @@
 import zerorpc
 import os
+import random
 from PastelCommon.keys import id_keypair_generation_func
+from PastelCommon.signatures import pastel_id_write_signature_on_data_func, \
+    pastel_id_verify_signature_with_public_key_func
+
+KEY_PATH = 'keys'
+
+
+def generate_key_id():
+    key_id = random.randint(10000, 99999)
+    while os.path.exists(os.path.join(KEY_PATH, 'private_{}.key'.format(key_id))):
+        key_id = random.randint(10000, 99999)
+    return key_id
 
 
 class Api:
@@ -8,15 +20,27 @@ class Api:
         # TODO: support custom path for keys, proccess different errors (file already
         # TOOD: exists, no access to file, etc..
         __privkey, __pubkey = id_keypair_generation_func()
-        privpath = 'private.key'
-        pubpath = 'public.key'
-        with open(privpath, "wb") as f:
+        key_id = generate_key_id()
+        privkey = 'private_{}.key'.format(key_id)
+        pubkey = 'public_{}.key'.format(key_id)
+        if not os.path.exists(KEY_PATH):
+            os.mkdir(KEY_PATH)
+        with open(os.path.join(KEY_PATH, privkey), "wb") as f:
             f.write(__privkey)
-        os.chmod(privpath, 0o0700)
-        with open(pubpath, "wb") as f:
+        os.chmod(os.path.join(KEY_PATH, privkey), 0o0700)
+        with open(os.path.join(KEY_PATH, pubkey), "wb") as f:
             f.write(__pubkey)
-        os.chmod(pubpath, 0o0700)
-        return 'Generated'
+        os.chmod(os.path.join(KEY_PATH, pubkey), 0o0700)
+        return {
+            'private': os.path.join(KEY_PATH, privkey),
+            'public': os.path.join(KEY_PATH, pubkey)
+        }
+
+    def sign_message(self, msg):
+        pass
+
+    def verify_signature(self, msg, signature, public_key):
+        pass
 
 
 def main():
