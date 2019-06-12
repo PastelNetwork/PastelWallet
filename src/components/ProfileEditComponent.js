b454a93189242dac5b28bf2c675b9f4468a2ae90
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-06-12 11:57:56 +0300

    Handle user input on the profile edit page. Process image change.

--- src/components/ProfileEditComponent.js
+++ src/components/ProfileEditComponent.js
@@ -3,108 +3,138 @@ import '../styles.scss';
 import '../assets/scss/core.scss';
 import '../assets/scss/custom.scss';
 import 'bulma/bulma.sass';
-import {BalancesContainer} from "../containers/dashboard/BalancesContainer";
-import {Profile} from "./dashboard/ProfileComponent";
 import {MainWrapper} from "./MainWrapperComponent";
-import history from "../history";
 import * as Feather from 'react-feather';
 
 
-const EditPicCard = (props) => {
-    return <div className="column is-5">
-        <div className="flat-card profile-card is-auto">
+class EditPicCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: null
+        };
+        this.fileInputRef = React.createRef();
+    }
+
+    onChangeFile = (e) => {
+        if (e.target.files.length) {
+            let file = e.target.files[0];
+            this.setState({file: URL.createObjectURL(file)});
+        }
+    };
+
+    fileAddClick = (e) => {
+        this.fileInputRef.current.click();
+    };
+
+    render() {
+        return <div className="flat-card upload-card is-auto">
             <div className="card-body">
-                <div className="profile-image" onClick={this.onImageClick}>
-                    <img src="https://i.pravatar.cc/200" alt=""/>
+                <div id="avatar-upload" className="avatar-wrapper has-simple-popover"
+                     data-content="Change profile picture" data-placement="top">
+                    <img className="profile-pic" src={this.state.file} alt=""/>
+                    <div className="upload-button" onClick={this.fileAddClick}>
+                        <Feather.Plus className="upload-icon" aria-hidden="true"/>
+                    </div>
+                    <input className="file-upload" type="file" accept="image/*" onChange={this.onChangeFile}
+                           ref={this.fileInputRef}/>
                 </div>
+
                 <div className="username has-text-centered">
                     <span>Elie Daniels</span>
-                    <small>Member since Sep 23 2017</small>
+                    <span>eliedaniels@gmail.com</span>
+                </div>
+
+                <div className="has-text-centered">
+                    <button className="button feather-button secondary-button will-upload">
+                        Save picture
+                    </button>
                 </div>
             </div>
         </div>
-    </div>
-};
+    }
+}
 
+class EditInfoCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phone: '',
+            email: ''
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+    render() {
+        return <div className="flat-card profile-info-card is-auto">
+            <div className="card-title">
+                <h3>Contact info</h3>
+                <div className="confirm-button">
+                    <a href="javascript:void(0);" role="button" className="has-simple-popover"
+                       data-content="Save Contact info" data-placement="top">
+                        <Feather.Check/>
+                    </a>
+                </div>
+            </div>
+            <div className="card-body">
+                <div className="columns">
+                    <div className="column is-6">
+                        <div className="info-block">
+                            <span className="label-text">First Name</span>
+                            <div className="control">
+                                <input type="text" className="input is-default" value={this.state.firstName}
+                                       onChange={this.onChange} name="firstName"/>
+                            </div>
+                        </div>
+
+                        <div className="info-block">
+                            <span className="label-text">Email</span>
+                            <div className="control">
+                                <input type="email" className="input is-default"
+                                       value={this.state.email}
+                                       onChange={this.onChange} name="email"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="column is-6">
+                        <div className="info-block">
+                            <span className="label-text">Last Name</span>
+                            <div className="control">
+                                <input type="text" className="input is-default" value={this.state.lastName}
+                                       onChange={this.onChange} name="lastName"/>
+                            </div>
+                        </div>
+
+                        <div className="info-block">
+                            <span className="label-text">Phone</span>
+                            <div className="control">
+                                <input type="text" className="input is-default" value={this.state.phone}
+                                       onChange={this.onChange} name="phone"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>;
+    }
+}
 
 export class ProfileEdit extends Component {
     render() {
         return <MainWrapper noLogo>
             <div className="columns is-account-grid is-multiline">
                 <div className="column is-4">
-                    <div className="flat-card upload-card is-auto">
-                        <div className="card-body">
-                            <div id="avatar-upload" className="avatar-wrapper has-simple-popover"
-                                 data-content="Change profile picture" data-placement="top">
-                                <img className="profile-pic" src="http://via.placeholder.com/250x250" alt=""/>
-                                <div className="upload-button">
-                                    <Feather.Plus className="upload-icon" aria-hidden="true"/>
-                                </div>
-                                <input className="file-upload" type="file" accept="image/*"/>
-                            </div>
-
-                            <div className="username has-text-centered">
-                                <span>Elie Daniels</span>
-                                <span>eliedaniels@gmail.com</span>
-                            </div>
-
-                            <div className="has-text-centered">
-                                <button className="button feather-button secondary-button will-upload">
-                                    Save picture
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    <EditPicCard/>
                 </div>
 
                 <div className="column is-8">
-                    <div className="flat-card profile-info-card is-auto">
-                        <div className="card-title">
-                            <h3>Contact info</h3>
-                            <div className="confirm-button">
-                                <a href="javascript:void(0);" role="button" className="has-simple-popover"
-                                   data-content="Save Contact info" data-placement="top">
-                                    <Feather.Check/>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            <div className="columns">
-                                <div className="column is-6">
-                                    <div className="info-block">
-                                        <span className="label-text">First Name</span>
-                                        <div className="control">
-                                            <input type="text" className="input is-default" value="Elie"/>
-                                        </div>
-                                    </div>
-
-                                    <div className="info-block">
-                                        <span className="label-text">Email</span>
-                                        <div className="control">
-                                            <input type="email" className="input is-default"
-                                                   value="eliedaniels@gmail.com"/>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="column is-6">
-                                    <div className="info-block">
-                                        <span className="label-text">Last Name</span>
-                                        <div className="control">
-                                            <input type="text" className="input is-default" value="Daniels"/>
-                                        </div>
-                                    </div>
-
-                                    <div className="info-block">
-                                        <span className="label-text">Phone</span>
-                                        <div className="control">
-                                            <input type="text" className="input is-default" value="+1 555 623 568"/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <EditInfoCard/>
                 </div>
             </div>
         </MainWrapper>;
