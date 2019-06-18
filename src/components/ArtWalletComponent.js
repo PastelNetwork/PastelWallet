1b77b80abc6d8e03343e95a19f0eda0ab3b4524f
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-06-18 11:12:43 +0300

    Continue cleanup

--- src/components/ArtWalletComponent.js
+++ src/components/ArtWalletComponent.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 import '../styles.scss';
 import {store} from '../app';
-import history from '../history';
 import {setBalance, setBlockchainData} from "../actions";
 import {RESPONSE_STATUS_OK} from "../constants";
-import * as PastelLogo from '../assets2/image/pastel_logo.png';
-import * as PastelFavicon from '../assets/images/favicon.png';
 import '../assets/scss/core.scss';
 import '../assets/scss/custom.scss';
 import 'bulma/bulma.sass';
 import {BalancesContainer} from "../containers/dashboard/BalancesContainer";
 import {Profile} from "./dashboard/ProfileComponent";
-import {SideBar} from "./common/SideBarComponent";
 import {MainWrapper} from "./MainWrapperComponent";
 
 const ipcRenderer = window.require('electron').ipcRenderer;
@@ -40,38 +36,11 @@ ipcRenderer.on('getBalanceResponse', (event, data) => {
 
 
 export class ArtWallet extends Component {
-    constructor(props) {
-        super(props);
-        this.fileInputRef = React.createRef();
-        this.state = {
-            file: null
-        }
-    }
-
     componentDidMount() {
         document.title = 'Pastel wallet';
         ipcRenderer.send('blockchainDataRequest', {});
         ipcRenderer.send('getBalanceRequest', {});
     }
-
-    onUploadClick = () => {
-        history.push('/register');
-        // this.fileInputRef.current.click();
-    };
-    onAddFile = (e) => {
-        let file = this.fileInputRef.current.files[0];
-        this.setState({file: URL.createObjectURL(file)});
-    };
-    onSubmitClick = (e) => {
-        let file = this.fileInputRef.current.files[0];
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = e.targer.result;
-            //TODO: push result to django backend
-            // axios.post()
-        };
-        reader.readAsBinaryString(file);
-    };
 
     render() {
         return <MainWrapper>
--- src/components/ArtistComponent.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-export class Artist extends Component {
-    render() {
-        return <div>Artist</div>;
-    }
-}
