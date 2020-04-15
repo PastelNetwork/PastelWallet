6d381acdba739c04b138c9c9ecdce65efc93beac
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-04-15 19:27:26 +0300

    Markup for artwork details page (WIP).

--- src/containers/ArtworkDetails/index.js
+++ src/containers/ArtworkDetails/index.js
@@ -4,19 +4,50 @@ import * as style from './style.module.scss';
 import { connect } from 'react-redux';
 import history from '../../history';
 
-class ArtworkDetails extends Component {
+const humanReadaableFieldNames = {
+  artistPastelId: 'Artist Pastel ID',
+  numOfCopies: 'Total copies',
+  copyPrice: ' Copy price',
+  artistWebsite: 'Artist website',
+  artistWrittenStatement: 'Artist written statement',
+  artworkSeriesName: 'Artwork series',
+  artworkCreationVideoYoutubeUrl: 'Creation video URL',
+  artworkKeywordSet: 'Keywords',
+  imageHash: 'Artwork image hash (SHA3-512 digest)',
+  blocknum: 'Registration ticket block number'
+};
+const dataFieldNames = ['artistPastelId', 'numOfCopies', 'copyPrice', 'artistWebsite', 'artistWrittenStatement', 'artworkSeriesName', 'artworkCreationVideoYoutubeUrl', 'artworkKeywordSet', 'imageHash', 'blocknum'];
 
-  render() {
+class ArtworkDetails extends Component {
+  render () {
     const imageHash = this.props.match.params.image_hash;
-    // fetch required artwork by image_hash
     let artwork = this.props.artworksData.filter(a => a.imageHash === imageHash)[0];
-    return <MainWrapper>
-      <button onClick={() => history.push('/artworks')}>Back</button>
-      <h1>{artwork.name}</h1>
-    </MainWrapper>;
+    const dataFields = dataFieldNames.map((item, idx) => {
+      return <div className={style['artwork-data-item']} key={idx}>
+        <div className={style['local-header']}>{humanReadaableFieldNames[item]}</div>
+        <div className={style['content']}>{artwork[item]}</div>
+      </div>;
+
+    });
+    return (
+      <MainWrapper>
+        <div className={style['image-wrapper']}>
+          <img src={`file://${artwork.thumbnailPath}`} alt=""/>
+        </div>
+        <div className={style['artwork-main-data']}>
+          <h1>{artwork.name}</h1>
+          <h5>Buy {artwork.artistName}</h5>
+          <p>TXID {artwork.orderBlockTxid}</p>
+        </div>
+        <div className={style['artwork-other-data']}>
+          {dataFields}
+        </div>
+
+      </MainWrapper>
+    );
   }
 }
 
 export default connect(state => ({
   artworksData: state.artworksData,
-}), dispatch => ({dispatch}))(ArtworkDetails);
+}), dispatch => ({ dispatch }))(ArtworkDetails);
