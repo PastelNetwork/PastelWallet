b08c056b0d9cd0ef3862cdde0544690384d0d534
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-04-16 16:30:14 +0300

    Display wallet address, masternode and artwork quantity on the home screen.

--- src/containers/pastel_id_selection/HomeContainer.js
+++ src/containers/pastel_id_selection/HomeContainer.js
@@ -1,8 +1,11 @@
 import {connect} from 'react-redux';
-import {PastelIdHome} from "../../components/pastel_id_selection/Home";
+import PastelIdHome from "../../components/pastel_id_selection/Home";
 
 
 const mapStateToProps = state => ({
+    blockchainAddress: state.blockchainAddress,
+    artworks: state.artworks,
+    masternodes: state.masternodes
 });
 
 const mapDispatchToProps = dispatch => ({
