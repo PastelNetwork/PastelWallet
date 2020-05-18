fb5c24b10c7f43ae8253a55d902646dc614c6a9a
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-05-19 00:46:27 +0300

    Artwork details data fields.

--- src/client/containers/Gallery/Detail/index.js
+++ src/client/containers/Gallery/Detail/index.js
@@ -22,27 +22,39 @@ import { BTN_TYPE_GREEN } from '../../../components/common/constants';
 //     'orderBlockTxid': artwork.order_block_txid
 // }
 
+const humanReadableFieldNames = {
+  artistPastelId: 'Artist Pastel ID',
+  numOfCopies: 'Total copies',
+  copyPrice: ' Copy price',
+  artistWebsite: 'Artist website',
+  artistWrittenStatement: 'Artist written statement',
+  artworkSeriesName: 'Artwork series',
+  artworkCreationVideoYoutubeUrl: 'Creation video URL',
+  artworkKeywordSet: 'Keywords',
+  imageHash: 'Artwork image hash (SHA3-512 digest)',
+  blocknum: 'Registration ticket block number'
+};
+const dataFieldNames = ['artistPastelId', 'numOfCopies', 'copyPrice', 'artistWebsite', 'artistWrittenStatement', 'artworkSeriesName', 'artworkCreationVideoYoutubeUrl', 'artworkKeywordSet', 'imageHash', 'blocknum'];
+
 class Detail extends Component {
   render () {
     const saleData = {
       forSale: true,
       price: 1232
     };
+
     const imageHash = this.props.match.params.image_hash;
     const data = this.props.artworksData ? this.props.artworksData.filter(a => a.imageHash === imageHash)[0] : {};
-    const { name, orderBlockTxid, thumbnailPath, artistPastelId } = data;
+    const { name, orderBlockTxid, thumbnailPath, artistPastelId, artistWrittenStatement } = data;
+    const isMy = artistPastelId === this.props.pastelId;
     return <Wrapper>
       <Card>
         <div onClick={() => history.goBack()} className={style.backlink}>{'<<'} Back to gallery</div>
 
         <div className={style.artwork}>
+          {isMy ? <div className={style['my-sticker']}>my</div> : null}
+          {saleData.forSale ? <div className={style['sale-sticker']}>for sale</div> : null}
           <p className={style.txid}><span>TXID</span> {orderBlockTxid}</p>
-      {
-        artistPastelId === this.props.pastelId ?
-        <div className={style['my-sticker']}>my</div> : null}
-      {saleData.forSale ?
-        <div className={style['sale-sticker']}>for sale</div> : null}
-
           <img src={`file://${thumbnailPath}`} alt=""/>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div>
@@ -51,8 +63,23 @@ class Detail extends Component {
                 {saleData.price} PSL
               </span> : null}
             </div>
-              <Button btnType={BTN_TYPE_GREEN} style={{ width: '145px', height: '37px', marginTop: '20px' }}>Sell artwork</Button>
+            <Button btnType={BTN_TYPE_GREEN} style={{ width: '145px', height: '37px', marginTop: '20px' }}>Sell
+              artwork</Button>
           </div>
+          <div style={{marginTop: '20px'}}>
+          {dataFieldNames.map((field, idx) => {
+              if (data[field] === '' || !data[field]) {
+                return null;
+              }
+              return <div className={style.data} key={idx}>
+                <h5>{field}</h5>
+                <p>{data[field]}</p>
+                {idx !== dataFieldNames.length - 1 ? <div className={style.line}/> : null}
+              </div>;
+            }
+          )}
+          </div>
+
         </div>
       </Card>
     </Wrapper>;
