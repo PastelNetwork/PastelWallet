e2f4ee4354fcb281917099d77843010484184aea
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-05-19 11:04:39 +0300

    Artwork details sell-buy dialogs.

--- src/client/containers/Gallery/Detail/index.js
+++ src/client/containers/Gallery/Detail/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Wrapper, Card, Button } from '../../../components/common';
+import { Wrapper, Card, Button, Input } from '../../../components/common';
 import * as style from './style.module.scss';
 import { connect } from 'react-redux';
 import history from '../../../history';
-import { BTN_TYPE_GREEN } from '../../../components/common/constants';
+import { BTN_TYPE_GREEN, BTN_TYPE_LIGHT_GREEN } from '../../../components/common/constants';
 import { getSampleSaleData } from '../index';
 
 // {
@@ -43,16 +43,47 @@ class Detail extends Component {
     super(props);
     this.state = {
       sellMode: false,
-      buyMode: false
-    }
+      buyMode: false,
+      price: 0
+    };
   }
 
   render () {
     const imageHash = this.props.match.params.image_hash;
     const data = this.props.artworksData ? this.props.artworksData.filter(a => a.imageHash === imageHash)[0] : {};
-    const {forSale, price} = data.saleData ? data.saleData : {};
+    const { forSale, price } = data.saleData ? data.saleData : {};
     const { name, orderBlockTxid, thumbnailPath, artistPastelId } = data;
     const isMy = artistPastelId === this.props.pastelId;
+    let dialog = null;
+    let button = isMy ?
+      <Button btnType={BTN_TYPE_GREEN} style={{ width: '145px', height: '37px', marginTop: '20px' }}
+              disabled={forSale} onClick={() => this.setState({ sellMode: true })}>Sell
+        artwork</Button> :
+      <Button btnType={BTN_TYPE_GREEN} style={{ width: '145px', height: '37px', marginTop: '20px' }}
+              disabled={!forSale} onClick={() => this.setState({ buyMode: true })}>Buy
+        artwork</Button>;
+    if (this.state.sellMode) {
+      button = null;
+      dialog = <React.Fragment>
+        <Input type="number" name={'price'} label={'Set price'}
+               containerStyle={{ width: '270px' }}
+               style={{width: '270px', lineHeight: '20px'}}
+               value={this.state.price} onChange={(e) => this.setState({ price: e.target.value })}
+        />
+        <Button btnType={BTN_TYPE_GREEN} style={{ width: '130px', height: '37px', marginTop: '5px', marginRight: '10px' }} onClick={() => console.log('Not implemented')}>Sell</Button>
+        <Button btnType={BTN_TYPE_LIGHT_GREEN} style={{ width: '130px', height: '37px', marginTop: '5px' }}
+        onClick={()=> this.setState({sellMode: false})}>Decline</Button>
+      </React.Fragment>;
+    }
+    if (this.state.buyMode) {
+      button = <div style={{display: 'flex'}}>
+        <p className={style['buy-confirm']}>Do you really want to buy this artwork?</p>
+        <Button btnType={BTN_TYPE_GREEN} style={{ width: '60px', height: '37px', marginTop: '20px', marginRight: '10px' }} onClick={() => console.log('Not implemented')}>Yes</Button>
+        <Button btnType={BTN_TYPE_LIGHT_GREEN} style={{ width: '60px', height: '37px', marginTop: '20px' }}
+        onClick={()=> this.setState({buyMode: false})}>No</Button>
+
+      </div>
+    }
     return <Wrapper>
       <Card>
         <div onClick={() => history.goBack()} className={style.backlink}>{'<<'} Back to gallery</div>
@@ -69,21 +100,21 @@ class Detail extends Component {
                 {price} PSL
               </span> : null}
             </div>
-            <Button btnType={BTN_TYPE_GREEN} style={{ width: '145px', height: '37px', marginTop: '20px' }}>Sell
-              artwork</Button>
+            {button}
           </div>
-          <div style={{marginTop: '20px'}}>
-          {dataFieldNames.map((field, idx) => {
-              if (data[field] === '' || !data[field]) {
-                return null;
+          {dialog}
+          <div style={{ marginTop: '20px' }}>
+            {dataFieldNames.map((field, idx) => {
+                if (data[field] === '' || !data[field]) {
+                  return null;
+                }
+                return <div className={style.data} key={idx}>
+                  <h5>{humanReadableFieldNames[field]}</h5>
+                  <p>{data[field]}</p>
+                  {idx !== dataFieldNames.length - 1 ? <div className={style.line}/> : null}
+                </div>;
               }
-              return <div className={style.data} key={idx}>
-                <h5>{field}</h5>
-                <p>{data[field]}</p>
-                {idx !== dataFieldNames.length - 1 ? <div className={style.line}/> : null}
-              </div>;
-            }
-          )}
+            )}
           </div>
 
         </div>
