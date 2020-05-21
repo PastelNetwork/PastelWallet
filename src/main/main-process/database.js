e7b81f957ea8151b1e09cf355d5c8d5b51651db5
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-05-21 17:18:30 +0300

    Save/fetch user profile to/from DB, WIP.

--- src/main/main-process/database.js
+++ src/main/main-process/database.js
@@ -8,7 +8,34 @@ let WALLET_DATABASE;
 
 export const SELECT_PASTELID_SQL = 'select pastelid from pastelid';
 
-const createTableSQL = 'CREATE TABLE IF NOT EXISTS pastelid (pastelid varchar);';
+export const GET_PROFILE_SQL = 'select firstName, lastName, phone, email, photo from profile where pastelid=?';
+
+// SQLite support UPSERT starting from version 3.24, but hoping that node-sqlite package has the right version is unreliable
+// So, old insert or ignore/update sql here.
+export const setProfileSQL = (data) => {
+  const { firstName, lastName, phone, email, photo, pastelid } = data;
+  return `INSERT OR IGNORE INTO profile 
+    (pastelid, firstName, lastName, phone, email, photo) 
+    VALUES (${pastelid}, ${firstName}, ${lastName}, ${phone}, ${email}, ${photo});
+    UPDATE profile SET firstName=${firstName}, lastName=${lastName}, phone=${phone}, email=${email}, photo=${photo} WHERE pastelid=${pastelid};`;
+};
+
+
+const createTableSQL = `
+    CREATE TABLE IF NOT EXISTS pastelid
+    (
+        pastelid varchar
+    );
+    CREATE TABLE IF NOT EXISTS profile
+    (
+        pastelid  varchar NOT NULL UNIQUE,
+        firstName varchar,
+        lastName  varchar,
+        phone     varchar,
+        email     varchar,
+        photo     varchar
+    );
+`;
 
 const getDbFilePath = () => path.join(getWorkDir(), 'wallet_.db');
 
--- src/main/main-process/profile.js
+++ src/main/main-process/profile.js
@@ -1,6 +1,20 @@
 import { ipcMain } from 'electron';
 import log from 'electron-log';
 import { RESPONSE_STATUS_ERROR, RESPONSE_STATUS_OK } from '../constants';
+import { GET_PROFILE_SQL, getDatabase, SELECT_PASTELID_SQL, setProfileSQL } from './database';
+
+const getProfileFromDB = (pastelid, fn) => {
+  const fields = ['firstName', 'lastName', 'phone', 'email', 'photo'];
+  getDatabase().all(GET_PROFILE_SQL, [pastelid], (e, r) => {
+    const profileObj = {};
+    fields.map((field, idx) => profileObj[field] = r[idx]);
+    fn(profileObj);
+  });
+};
+
+const saveProfile = (data, fn) => {
+  getDatabase().run(setProfileSQL(data), [], (e, r) => fn());
+};
 
 const getProfileHandler = (event, data) => {
   // TODO: fetch profile from DB using pastel ID (data.pastelid)
