b61009b76e8b5de67f31788bdf5ed99a2ff27db4
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-10-14 13:11:05 +0300

    Fix empty profile exception

--- src/client/ipc/profile.js
+++ src/client/ipc/profile.js
@@ -1,15 +1,17 @@
-import { RESPONSE_STATUS_OK } from '../constants';
-import { store } from '../app';
-import { SET_USER_PROFILE_DATA } from '../actionTypes';
-import { ipcRenderer } from './ipc';
+import {RESPONSE_STATUS_OK} from '../constants';
+import {store} from '../app';
+import {SET_USER_PROFILE_DATA} from '../actionTypes';
+import {ipcRenderer} from './ipc';
 import history from '../history';
 
 ipcRenderer.on('getProfileResponse', (event, data) => {
   if (data.status === RESPONSE_STATUS_OK) {
     console.log(data);
-    Object.keys(data.data).map(field =>
-      store.dispatch({ type: SET_USER_PROFILE_DATA, field, value: data.data[field] })
-    );
+    if (data.data) {
+      Object.keys(data.data).map(field =>
+        store.dispatch({type: SET_USER_PROFILE_DATA, field, value: data.data[field]})
+      );
+    }
   } else {
     console.log('Error fetching user profile data');
   }
@@ -19,7 +21,7 @@ ipcRenderer.on('saveProfileResponse', (event, data) => {
   if (data.status === RESPONSE_STATUS_OK) {
     console.log(data);
     Object.keys(data.data).map(field =>
-      store.dispatch({ type: SET_USER_PROFILE_DATA, field, value: data.data[field] })
+      store.dispatch({type: SET_USER_PROFILE_DATA, field, value: data.data[field]})
     );
     history.goBack();
   } else {
